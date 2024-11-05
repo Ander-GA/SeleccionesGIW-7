45f17b4384889659ea706a5cb4348be8d274553c
Emilio Gil del Rio <emilio.gildelrio@opendeusto.es>
2024-11-05 13:25:45 +0100

    Update admin.py

--- ProjectoSeleccionesGIW7/seleccionesAPP/admin.py
+++ ProjectoSeleccionesGIW7/seleccionesAPP/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Confederacion, Seleccion, Futbolista
+
+admin.site.register(Confederacion)
+admin.site.register(Seleccion)
+admin.site.register(Futbolista)
