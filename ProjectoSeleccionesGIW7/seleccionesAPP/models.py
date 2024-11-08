86aa0070df54b014220289fe76fde7ce185bba00
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-08 17:27:14 +0100

    html y model

--- ProjectoSeleccionesGIW7/seleccionesAPP/models.py
+++ ProjectoSeleccionesGIW7/seleccionesAPP/models.py
@@ -4,21 +4,22 @@ from django.db import models
 class Confederacion(models.Model):
     nombre = models.CharField(max_length=100)
     continente = models.CharField(max_length=50) 
-    
-    def __str__(self):
-        return self.name
 
+    def __str__(self):
+        return self.nombre
 class Seleccion(models.Model):
     nombre = models.CharField(max_length=100)
     confederacion = models.ForeignKey(Confederacion, on_delete=models.CASCADE, related_name='seleccion')
-                                      
+    
     def __str__(self):
-        return self.name
+        return self.nombre                                     
 
 class Futbolista(models.Model):
     nombre = models.CharField(max_length=100)
     edad = models.IntegerField()
     posicion = models.CharField(max_length=50)  # Ej: Portero, Defensa, Mediocampista, Delantero
     seleccion = models.ForeignKey(Seleccion, on_delete=models.CASCADE, related_name='futbolista')
+    
     def __str__(self):
-        return self.name
+        return self.nombre
+    
