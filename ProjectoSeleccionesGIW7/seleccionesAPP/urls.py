d65fb6ecfee91a7a4785a0d1732fc50fe7f05aff
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-08 11:48:31 +0100

    views y urls

--- ProjectoSeleccionesGIW7/seleccionesAPP/urls.py
+++ ProjectoSeleccionesGIW7/seleccionesAPP/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include 
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    path('seleccionesAPP/', include('seleccionesAPP.urls')), 
+    path('inicio',views.index,name='index'),
 
+    #Listado de confederaciones
+    path('listadoDeConfederaciones', views.listaConfederacionConPlantillas,name='listaC'),
+    #Detalle de confederaciones
+    path('listadoDeConfederaciones/<int:id_confederacion', views.detalleConfederacionConPlantillas,name='detalleC'),
+    #Listado de selecciones
+    path('listadoDeSelecciones', views.listaSeleccionPlantillas,name='listaS'),
+    #Detalle de seleccion
+    path('listadoDeSelecciones/<int:id_seleccion', views.detalleSeleccionPlantillas,name='detalleS'),
+    #Listado de futbolistas
+    path('listadoDeFutbolistas', views.listaFutbolistaConPlantillas,name='listaF'),
+    #Detalle de futbolista
+    path('listadoDeFutbolistas/<int:id_futbolista', views.detalleFutbolistaConPlantillas,name='detalleF'),
 ]
--- ProjectoSeleccionesGIW7/seleccionesAPP/views.py
+++ ProjectoSeleccionesGIW7/seleccionesAPP/views.py
@@ -1,67 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Confederacion, Seleccion, Futbolista
 
 # Vistas de lista
-def confederacion_list(request):
-    confederaciones = Confederacion.objects.all()
-    return render(request, 'confederacion_list.html', {'confederaciones': confederaciones})
+def index():
+    return HttpResponse('primera vista')
 
-def seleccion_list(request):
-    selecciones = Seleccion.objects.all()
-    return render(request, 'seleccion_list.html', {'selecciones': selecciones})
+#SELECCIONES 
+def listaSeleccionPlantillas(request):
+    seleccion = Seleccion.objects.order_by('nombre')
+    contexto = {'seleccion_list': seleccion}
+    return render(request, 'listaSeleccion.html', contexto)
 
-def futbolista_list(request):
-    futbolistas = Futbolista.objects.all()
-    return render(request, 'futbolista_list.html', {'futbolistas': futbolistas})
+def detalleSeleccionPlantillas(request, id_seleccion):
+    seleccion = get_object_or_404(Seleccion, pk=id_seleccion)
+    contexto = {'seleccion': seleccion}
+    return render(request, 'detalleSeleccion.html', contexto)
 
-# Vistas de detalle
-def confederacion_detalle(request, id_confederacion):
-    try:
-        confederacion = Confederacion.objects.get(pk=id_confederacion)
-        selecciones = confederacion.selecciones.all()
-        
-        # Crear la cadena de texto con los detalles de la confederación
-        cadenaDeTexto = f"{confederacion.nombre} - CIF: {confederacion.cif}\n"
-        # Listar selecciones
-        if selecciones.exists():
-            cadenaDeTexto += "Selecciones:\n"
-            for seleccion in selecciones:
-                cadenaDeTexto += f"- {seleccion.nombre}\n"
-        else:
-            cadenaDeTexto += "No hay selecciones asociadas a esta confederación."
-            
-        return HttpResponse(cadenaDeTexto)
-    except Confederacion.DoesNotExist:
-        return HttpResponseNotFound("Confederación no encontrada")
+#JUGADORES
+def listaFutbolistaConPlantillas(request):
+    futbolistas = Futbolista.objects.order_by('nombre')
+    contexto = {'futbolista_list' : futbolistas}
+    return render(request, 'listaFutbolista.html', contexto)
 
-# Vista de detalle para Seleccion
-def detalle_seleccion(request, id_seleccion):
-    try:
-        seleccion = Seleccion.objects.get(pk=id_seleccion)
-        futbolistas = seleccion.futbolistas.all() 
-        # Crear la cadena de texto con los detalles de la selección
-        cadenaDeTexto = f"{seleccion.nombre} - CIF: {seleccion.cif}\n"
-        # Listar futbolistas
-        if futbolistas.exists():
-            cadenaDeTexto += "Futbolistas:\n"
-            for futbolista in futbolistas:
-                cadenaDeTexto += f"- {futbolista.nombre}, Posición: {futbolista.posicion}\n"
-        else:
-            cadenaDeTexto += "No hay futbolistas asociados a esta selección."
-        return HttpResponse(cadenaDeTexto)
-    except Seleccion.DoesNotExist:
-        return HttpResponseNotFound("Selección no encontrada")
+def detalleFutbolistaConPlantillas(request, id_futbolista):
+    futbolista = get_object_or_404(Futbolista, pk=id_futbolista)
+    contexto = {'futbolista': futbolista}
+    return render(request, 'detalleProfesor.html', contexto)
 
-def futbolista_detail(request, id_futbolisa):
-    try:
-        futbolista = Futbolista.objects.get(pk=id_futbolisa)
-        #Crear la cadena de texto con los detalles de la selección
-        cadenaDeTexto = f"{futbolista.nombre} - Código: {futbolista.codigo}\n"
-        if futbolista.exist():
-            cadenaDeTexto += f"- {futbolista.nombre}, Posición: {futbolista.posicion}\n"
-        else:
-            cadenaDeTexto += "No hay futbolistas asociado."
-        return HttpResponse(cadenaDeTexto)
-    except Seleccion.DoesNotExist:
-        return HttpResponseNotFound("Futbolista no encontrado")
+#CONFEDERACION
+def listaConfederacionConPlantillas(request):
+    ciudad = Confederacion.objects.order_by('nombre')
+    contexto = {'ciudad_list' : ciudad}
+    return render(request, 'listaCiudad.html', contexto)
+
+def detalleConfederacionConPlantillas(request, id_ciudad):
+    confederacion = get_object_or_404(Confederacion, pk=id_ciudad)
+    contexto = {'confederacion': confederacion}
+    return render(request, 'detalleConfederacion.html', contexto)
+
+
