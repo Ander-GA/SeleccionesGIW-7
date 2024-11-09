6299b78f9454cd6045fb51584d404971433b76eb
Ander Gonzalez <ander.gonzalez.a@opendeusto.es>
2024-11-09 19:08:31 +0100

    cambio de las urls de detalle

--- ProjectoSeleccionesGIW7/seleccionesAPP/urls.py
+++ ProjectoSeleccionesGIW7/seleccionesAPP/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
     #Listado de confederaciones
     path('listadoDeConfederaciones', views.listaConfederacionConPlantillas,name='listaC'),
     #Detalle de confederaciones
-    path('listadoDeConfederaciones/<int:id_confederacion', views.detalleConfederacionConPlantillas,name='detalleC'),
+    path('detalleDeConfederaciones/<int:id_confederacion', views.detalleConfederacionConPlantillas,name='detalleC'),
     #Listado de selecciones
     path('listadoDeSelecciones', views.listaSeleccionPlantillas,name='listaS'),
     #Detalle de seleccion
-    path('listadoDeSelecciones/<int:id_seleccion', views.detalleSeleccionPlantillas,name='detalleS'),
+    path('detalleDeSelecciones/<int:id_seleccion', views.detalleSeleccionPlantillas,name='detalleS'),
     #Listado de futbolistas
     path('listadoDeFutbolistas', views.listaFutbolistaConPlantillas,name='listaF'),
     #Detalle de futbolista
-    path('listadoDeFutbolistas/<int:id_futbolista', views.detalleFutbolistaConPlantillas,name='detalleF'),
+    path('detalleDeFutbolistas/<int:id_futbolista', views.detalleFutbolistaConPlantillas,name='detalleF'),
 ]
